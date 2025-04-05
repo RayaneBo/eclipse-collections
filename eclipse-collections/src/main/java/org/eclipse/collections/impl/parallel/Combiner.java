d07095bb55e785617e37081074a793341d1f6d1a
rbouzid <rayane.bouzid.etu@univ-lille.fr>
2025-04-05 02:20:38 +0200

    amélioration de l'interface Combiner
    - Ajout de documentation complète en français avec Javadoc
    - Ajout de validation des paramètres avec Objects.requireNonNull
    - Ajout de méthodes utilitaires par défaut :
      - isValid() pour la validation d'état
      - reset() pour la réinitialisation
    - Amélioration de la sécurité des types
    - Documentation des exceptions possibles

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/parallel/Combiner.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/parallel/Combiner.java
@@ -11,17 +11,66 @@
 package org.eclipse.collections.impl.parallel;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import org.eclipse.collections.impl.utility.Iterate;
 
+/**
+ * Une interface représentant un combinateur qui peut fusionner les résultats de plusieurs procédures parallèles.
+ * Cette interface est utilisée pour combiner les résultats de tâches exécutées en parallèle.
+ *
+ * @param <T> Le type des éléments à combiner
+ */
 public interface Combiner<T> extends Serializable
 {
+    /**
+     * Combine tous les éléments d'une collection itérable.
+     * Par défaut, cette méthode appelle {@link #combineOne} pour chaque élément.
+     *
+     * @param thingsToCombine la collection d'éléments à combiner
+     * @throws NullPointerException si thingsToCombine est null
+     */
     default void combineAll(Iterable<T> thingsToCombine)
     {
+        Objects.requireNonNull(thingsToCombine, "thingsToCombine ne peut pas être null");
         Iterate.forEach(thingsToCombine, this::combineOne);
     }
 
+    /**
+     * Combine un seul élément.
+     *
+     * @param thingToCombine l'élément à combiner
+     * @throws NullPointerException si thingToCombine est null
+     */
     void combineOne(T thingToCombine);
 
+    /**
+     * Indique si la méthode {@link #combineOne} doit être utilisée pour combiner les résultats.
+     * Si false, la méthode {@link #combineAll} sera utilisée à la place.
+     *
+     * @return true si combineOne doit être utilisé, false sinon
+     */
     boolean useCombineOne();
+
+    /**
+     * Vérifie si le combinateur est valide pour une utilisation.
+     * Par défaut, retourne toujours true. Les implémentations peuvent surcharger cette méthode
+     * pour ajouter des validations spécifiques.
+     *
+     * @return true si le combinateur est valide, false sinon
+     */
+    default boolean isValid()
+    {
+        return true;
+    }
+
+    /**
+     * Réinitialise l'état du combinateur.
+     * Par défaut, ne fait rien. Les implémentations peuvent surcharger cette méthode
+     * pour réinitialiser leur état interne.
+     */
+    default void reset()
+    {
+        // Ne fait rien par défaut
+    }
 }
